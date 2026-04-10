8454965d0d4376010bcb95644f78e8198853c9b8
navrajsingh054 <164318896+navrajsingh054@users.noreply.github.com>
2026-04-10 07:40:48 +0530

    Implement function for subsets generation

---
+++ recursion/subsets2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void getallsubsets(vector<int> &nums,int i,vector<int> &ans,vector<vector<int>> &allsubsets){
+        if(i == nums.size()){
+            allsubsets.push_back(ans);
+            return;
+        }
+        ans.push_back(nums[i]);
+        getallsubsets(nums,i+1,ans,allsubsets);
+        ans.pop_back();
+        int idx = i+1;
+        while((idx < nums.size()) && (nums[idx] == nums[idx-1])){
+            idx++;
+        }
+        getallsubsets(nums,idx,ans,allsubsets);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> allsubsets;
+        vector<int>ans;
+        getallsubsets(nums,0,ans,allsubsets);
+        return allsubsets;
+    }
+};
