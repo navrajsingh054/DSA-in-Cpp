dbe683a19f126a709ada84af3e4d9137634a138a
navrajsingh054 <164318896+navrajsingh054@users.noreply.github.com>
2026-06-12 17:43:03 +0530

    Add files via upload

---
+++ Fundamentals/power_of2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n == 0){
+        cout<<"Not Power of 2"<<endl;
+        return 0;
+    }
+   while(n % 2 == 0){
+    n/=2;
+   }
+   if(n == 1){
+    cout<<"Power of 2"<<endl;
+   }
+   else {
+    cout<<"Not Power of 2"<<endl;
+   }
+   return 0;
+}
